3474865dbdc9711a13eb25e22f408dfb173fc509
Son-Tr <sontran.kh1502@gmail.com>
2024-06-10 20:52:25 +0700

    lesson 13

--- Lesson 13 Learn Functional Programming by Building a Spreadsheet/script.js
+++ Lesson 13 Learn Functional Programming by Building a Spreadsheet/script.js
@@ -12,3 +12,16 @@ const infixToFunction = {
     const str2 = infixEval(str, regex);
     return str === str2 ? str : highPrecedence(str2);
   }
+
+  const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+
+const median = nums => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
